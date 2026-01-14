8d94ff7e3e57ce2c6a0d1cb61f583986f8eacd46
Tomoya Fukuda <2311140073n@ed.fuk.kindai.ac.jp>
2026-01-14 07:16:30 +0000

    機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<
-ActiveRecord::Schema[8.1].define(version: 2025_12_12_123853) do
-
+ActiveRecord::Schema[8.1].define(version: 2025_12_17_081636) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
